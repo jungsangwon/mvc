522da547371c7fedc7d6699c9c38af8f4af3c4cd
jungsangwon <top1tiger@naver.com>
2023-03-07 01:09:42 +0900

    servlet & jsp

---
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,48 @@
+package hello.servlet.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach(){
+        memberRepository.clearStroe();
+    }
+
+    @Test
+    void save(){
+        //given
+        Member member = new Member("hello",20);
+
+        //when
+        Member savedMember = memberRepository.svae(member);
+
+        //then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    void findAll(){
+        //given
+        Member member1 = new Member("member1",20);
+        Member member2 = new Member("member2",30);
+
+        memberRepository.svae(member1);
+        memberRepository.svae(member2);
+
+        //when
+        List<Member> result = memberRepository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1,member2);
+    }
+}
